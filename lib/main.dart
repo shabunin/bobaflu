05e989b9433eba52982eb5a76e0b5022bcdd5db6
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2019-01-03 03:01:18 +0300

    lib/bobaos.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mdns/mdns.dart';
 
-// from dartpg
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+
+import './bobaos.dart';
 
 void main() => runApp(MyApp());
 
@@ -161,180 +162,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class BobaosCallback {
-  String name;
-  Function cb;
-
-  BobaosCallback(String name, Function cb) {
-    this.name = name;
-    this.cb = cb;
-  }
-}
-
-class BobaosWs {
-  String url;
-  WebSocket ws;
-
-  int _req_count;
-  Map _reqs;
-
-  // callbacks for bcasted events
-  List<BobaosCallback> _events;
-
-  void registerListener(String name, Function cb) {
-    this._events.add(new BobaosCallback(name, cb));
-  }
-
-  void removeAllListeners() {
-    this._events = [];
-  }
-
-  void emitEvent(String name, dynamic params) {
-    // call all listeners
-    List<BobaosCallback> foundCallbacks =
-        this._events.where((t) => t.name == name).toList();
-    foundCallbacks.forEach((f) => f.cb(params));
-  }
-
-  void initWs() async {
-    try {
-      this.ws = await WebSocket.connect(this.url);
-      print("readyState: ${this.ws.readyState}");
-      this.listen();
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  void closeWs() async {
-    try {
-      print("closin websocket");
-      this.removeAllListeners();
-      await this.ws.close(1, "done");
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  void listen() {
-    this.ws.listen((text) {
-      var json = jsonDecode(text);
-      if (json.containsKey('response_id')) {
-        var response_id = json['response_id'];
-        if (this._reqs.containsKey(response_id)) {
-          var cb = this._reqs[response_id];
-          if (json['method'] == 'success') {
-            cb(false, json['payload']);
-          } else {
-            cb(true, json['payload']);
-          }
-          this._reqs.remove(response_id);
-          //print('after removing ${this._reqs}');
-        }
-      } else {
-        // broadcasted event
-        this.emitEvent(json['method'], json['payload']);
-      }
-    });
-  }
-
-  void getGeneralInfo(Function cb) async {
-    print("readyState: ${this.ws.readyState}");
-    if (this.ws.readyState == 0) {
-      await this.initWs();
-    }
-    this._req_count += 1;
-    int request_id = this._req_count;
-    var obj2send = {};
-    obj2send["request_id"] = request_id;
-    obj2send["method"] = "get general info";
-    obj2send["payload"] = null;
-    print('sending ${jsonEncode(obj2send)}');
-    this.ws.add(jsonEncode(obj2send));
-    this._reqs[request_id] = cb;
-  }
-
-  void getAccessoryInfo(dynamic id, Function cb) async {
-    print("readyState: ${this.ws.readyState}");
-    if (this.ws.readyState == 0) {
-      await this.initWs();
-    }
-    this._req_count += 1;
-    int request_id = this._req_count;
-    var obj2send = {};
-    obj2send["request_id"] = request_id;
-    obj2send["method"] = "get accessory info";
-    obj2send["payload"] = id;
-    print('sending ${jsonEncode(obj2send)}');
-    this.ws.add(jsonEncode(obj2send));
-    this._reqs[request_id] = cb;
-  }
-
-  void getStatusValue(dynamic id, dynamic status, Function cb) async {
-    print("readyState: ${this.ws.readyState}");
-    if (this.ws.readyState == 0) {
-      await this.initWs();
-    }
-    this._req_count += 1;
-    int request_id = this._req_count;
-    var obj2send = {};
-    obj2send["request_id"] = request_id;
-    obj2send["method"] = "get status value";
-
-    var payload = {};
-    payload["id"] = id;
-    List<Object> statusList = [];
-    if (status is List) {
-      status.forEach((e) {
-        statusList.add(e);
-      });
-      payload['status'] = statusList;
-    } else {
-      payload['status'] = status;
-    }
-    obj2send["payload"] = payload;
-    print('sending ${jsonEncode(obj2send)}');
-    this.ws.add(jsonEncode(obj2send));
-    this._reqs[request_id] = cb;
-  }
-
-  void controlAccessoryValue(
-      dynamic id, Map<dynamic, dynamic> value, Function cb) async {
-    print("readyState: ${this.ws.readyState}");
-    if (this.ws.readyState == 0) {
-      await this.initWs();
-    }
-    this._req_count += 1;
-    int request_id = this._req_count;
-    var obj2send = {};
-    obj2send["request_id"] = request_id;
-    obj2send["method"] = "control accessory value";
-
-    List<Object> valueList = [];
-    value.forEach((k, v) {
-      valueList.add({'field': k, 'value': v});
-    });
-    var payload = {};
-    payload["id"] = id;
-    if (valueList.length == 1) {
-      payload["control"] = valueList[0];
-    } else {
-      payload["control"] = valueList;
-    }
-    obj2send["payload"] = payload;
-    print('sending ${jsonEncode(obj2send)}');
-    this.ws.add(jsonEncode(obj2send));
-    this._reqs[request_id] = cb;
-  }
-
-  BobaosWs(String url) {
-    this.url = url;
-    this._reqs = new Map<int, Function>();
-    this._req_count = 0;
-    this._events = new List<BobaosCallback>();
-  }
-}
-
 class AccessoryListPage extends StatefulWidget {
   AccessoryListPage({Key key, this.title, this.host}) : super(key: key);
 
@@ -414,42 +241,40 @@ class _AccessoryListPage extends State<AccessoryListPage> {
     });
 
     await bobaos.initWs();
-    new Timer(const Duration(seconds: 0), () {
-      bobaos.getAccessoryInfo(null, (bool err, Object payload) {
-        if (err) {
-          return print('error ocurred $payload');
-        }
+    bobaos.getAccessoryInfo(null, (bool err, Object payload) {
+      if (err) {
+        return print('error ocurred $payload');
+      }
 
-        print(payload);
-        List<Object> tmpList = payload;
-        setState(() {
-          tmpList.forEach((t) {
-            AccessoryInfo f = new AccessoryInfo(t);
-            bobaos.getStatusValue(f.id, f.status, (bool err, dynamic payload) {
-              dynamic statusValues = payload['status'];
-              print("get all statuses!!");
-              print(statusValues.toString());
-              if (statusValues is Map) {
-                dynamic field = statusValues['field'];
-                dynamic value = statusValues['value'];
-                f.currentState[field] = value;
-              }
-              if (statusValues is List) {
-                statusValues.forEach((statusValue) {
-                  if (statusValue is Map) {
-                    dynamic field = statusValue['field'];
-                    dynamic value = statusValue['value'];
-                    f.currentState[field] = value;
-                  }
-                });
-              }
-              setState(() {
-                accessoryList.add(f);
+      print(payload);
+      List<Object> tmpList = payload;
+      setState(() {
+        tmpList.forEach((t) {
+          AccessoryInfo f = new AccessoryInfo(t);
+          bobaos.getStatusValue(f.id, f.status, (bool err, dynamic payload) {
+            dynamic statusValues = payload['status'];
+            print("get all statuses!!");
+            print(statusValues.toString());
+            if (statusValues is Map) {
+              dynamic field = statusValues['field'];
+              dynamic value = statusValues['value'];
+              f.currentState[field] = value;
+            }
+            if (statusValues is List) {
+              statusValues.forEach((statusValue) {
+                if (statusValue is Map) {
+                  dynamic field = statusValue['field'];
+                  dynamic value = statusValue['value'];
+                  f.currentState[field] = value;
+                }
               });
+            }
+            setState(() {
+              accessoryList.add(f);
             });
           });
-          // get status values for each accessory
         });
+        // get status values for each accessory
       });
     });
   }
