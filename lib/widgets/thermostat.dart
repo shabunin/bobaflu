db841f53599b1511e279b1eee9ea5000626508a4
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2019-01-11 17:43:12 +0300

    thermostat works in progress

--- lib/widgets/thermostat.dart
+++ lib/widgets/thermostat.dart
@@ -40,6 +40,23 @@ class _AccThermostat extends State<AccThermostat> {
   @override
   Widget build(BuildContext context) {
     var cardColor = Theme.of(context).cardColor;
+    dynamic currentMode = info.currentState['mode'];
+    switch (currentMode) {
+      case "off":
+        cardColor = Colors.grey;
+        break;
+      case "heat":
+        cardColor = Colors.amber;
+        break;
+      case "cool":
+        cardColor = Colors.lightBlue;
+        break;
+      case "auto":
+        cardColor = Colors.green;
+        break;
+      default:
+        break;
+    }
 
     return Card(
         color: cardColor,
@@ -49,9 +66,7 @@ class _AccThermostat extends State<AccThermostat> {
           title: new Text("${info.name}"),
           subtitle: new Text(
               "${info.currentState['current temperature']} >> ${info.currentState['setpoint']}"),
-          // TODO: mode icon/color
-          trailing:
-              new CircleAvatar(child: new Text("${info.currentState['mode']}")),
+          trailing: new CircleAvatar(child: new Text("${currentMode[0]}")),
           onTap: () {
             // TODO:  with additional funcs
             Navigator.of(context).push(MaterialPageRoute(
@@ -85,78 +100,184 @@ class _AccThermostatControl extends State<AccThermostatControl> {
     AccessoryInfo info = widget.info;
     BobaosWs bobaos = widget.bobaos;
     void changeSetpointBy(double value) {
-      bobaos.getStatusValue(info.id, "setpoint",
+      bobaos.getStatusValue(info.id, "setpoint", (bool err, Object payload) {
+        if (err) {
+          return print('error ocurred $payload');
+        }
+
+        print(payload);
+        if (payload is Map) {
+          dynamic currentValue = payload['status']['value'];
+          dynamic newValue = currentValue + value;
+          bobaos.controlAccessoryValue(info.id, {"setpoint": newValue},
               (bool err, Object payload) {
             if (err) {
-              return print(
-                  'error ocurred $payload');
+              return print('error ocurred $payload');
             }
 
             print(payload);
-            if (payload is Map) {
-              dynamic currentValue =
-              payload['status']['value'];
-              dynamic newValue = currentValue + value;
-              bobaos.controlAccessoryValue(
-                  info.id, {"setpoint": newValue},
-                      (bool err, Object payload) {
-                    if (err) {
-                      return print(
-                          'error ocurred $payload');
-                    }
-
-                    print(payload);
-                  });
-            }
           });
+        }
+      });
     }
+
     return new Scaffold(
         appBar: AppBar(
-          title: Text(widget.info.name),
+          title: Text(info.name),
         ),
         body: ScopedModel<AccessoryInfo>(
             model: info,
             child: ListView(
               children: <Widget>[
-                // setpoint
-                SizedBox(
-                    height: 210,
-                    child: Card(
-                        color: Colors.amber,
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: SizedBox(
-                                  height: 210,
-                                  child: InkWell(
-                                    child: Icon(Icons.remove_circle),
-                                    onTap: () {
-                                      changeSetpointBy(-0.5);
-                                    },
-                                  )),
-                            ),
-                            Expanded(child:
-                                ScopedModelDescendant<AccessoryInfo>(
-                                    builder: (context, child, model) {
-                              return Center(
-                                  child: Text(
-                                      "${model.currentState['setpoint']}",
-                                      style: TextStyle(
-                                          fontSize: 42,
-                                          fontWeight: FontWeight.w900)));
-                            })),
-                            Expanded(
-                              child: SizedBox(
-                                  height: 210,
-                                  child: InkWell(
-                                    child: Icon(Icons.add_circle),
-                                    onTap: () {
-                                      changeSetpointBy(0.5);
-                                    },
-                                  )),
-                            )
-                          ],
-                        )))
+                ScopedModelDescendant<AccessoryInfo>(
+                    builder: (context, child, model) {
+                  var cardColor;
+                  dynamic currentMode = model.currentState['mode'];
+                  switch (currentMode) {
+                    case "off":
+                      cardColor = Colors.grey;
+                      break;
+                    case "heat":
+                      cardColor = Colors.amber;
+                      break;
+                    case "cool":
+                      cardColor = Colors.lightBlue;
+                      break;
+                    case "auto":
+                      cardColor = Colors.green;
+                      break;
+                    default:
+                      break;
+                  }
+                  return Column(children: <Widget>[
+                    SizedBox(
+                        height: 64,
+                        child: Card(
+                            color: cardColor,
+                            child: Center(
+                                child: Text(
+                                    "Temperature now: ${model.currentState['current temperature']}",
+                                    style: TextStyle(
+                                        fontSize: 21,
+                                        fontWeight: FontWeight.w700))))),
+
+                    // setpoint
+                    SizedBox(
+                        height: 120,
+                        child: Card(
+                            color: cardColor,
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: SizedBox(
+                                      height: 120,
+                                      child: InkWell(
+                                        child: Icon(Icons.remove_circle),
+                                        onTap: () {
+                                          changeSetpointBy(-1);
+                                        },
+                                      )),
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    Text("Setpoint:",
+                                        style: TextStyle(
+                                            fontSize: 21,
+                                            fontWeight: FontWeight.w900)),
+                                    Expanded(
+                                      child: Center(
+                                          child: Text(
+                                              "${model.currentState['setpoint']}",
+                                              style: TextStyle(
+                                                  fontSize: 42,
+                                                  fontWeight:
+                                                      FontWeight.w900))),
+                                    )
+                                  ],
+                                ),
+                                Expanded(
+                                  child: SizedBox(
+                                      height: 120,
+                                      child: InkWell(
+                                        child: Icon(Icons.add_circle),
+                                        onTap: () {
+                                          changeSetpointBy(1);
+                                        },
+                                      )),
+                                )
+                              ],
+                            ))),
+                  ]);
+                }),
+
+                // modes
+                ScopedModelDescendant<AccessoryInfo>(
+                    builder: (context, child, model) {
+                  dynamic currentMode = model.currentState['mode'];
+                  List<dynamic> modes = model.currentState['modes available'];
+                  return SizedBox(
+                      height: 48,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: modes.length,
+                          itemBuilder: (BuildContext ctx, int index) {
+                            return Row(children: <Widget>[
+                              Radio(
+                                value: modes[index],
+                                groupValue: currentMode,
+                                onChanged: (dynamic value) {
+                                  bobaos.controlAccessoryValue(
+                                      info.id, {"mode": value},
+                                      (bool err, Object payload) {
+                                    if (err) {
+                                      return print('error ocurred $payload');
+                                    }
+
+                                    print(payload);
+                                  });
+                                },
+                              ),
+                              Text("${modes[index]}")
+                            ]);
+                          }));
+                }),
+
+                // sensors
+                ScopedModelDescendant<AccessoryInfo>(
+                    builder: (context, child, model) {
+                  return Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Text(
+                              "Temperature: ${model.currentState['sensors']['temperature']}")),
+                      Expanded(
+                          child: Text(
+                              "Humidity: ${model.currentState['sensors']['humidity']}")),
+                      Expanded(
+                          child: Text(
+                              "CO2: ${model.currentState['sensors']['co2']}")),
+                    ],
+                  );
+                }),
+
+                // error state
+                ScopedModelDescendant<AccessoryInfo>(
+                    builder: (context, child, model) {
+                  var errorState = model.currentState['error state']['error'];
+                  if (errorState is bool) {
+                    if (errorState) {
+                      return Card(
+                          color: Colors.red,
+                          child: Text(
+                            "${model.currentState['error state']['message']}",
+                            style: TextStyle(
+                                fontSize: 42, fontWeight: FontWeight.w900),
+                          ));
+                    }
+                  }
+
+                  return new Container();
+                }),
               ],
             )));
   }
